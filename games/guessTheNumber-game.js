10b82fc5a8bd6bc912ee5e76c83ea803245e7813
Dream-prince <77263344+DreamBoy65@users.noreply.github.com>
2021-12-27 13:50:11 +0530

    Update guessTheNumber-game.js

--- games/guessTheNumber-game.js
+++ games/guessTheNumber-game.js
@@ -1,28 +1,41 @@
 class GuessTheNumber {
 
     constructor(options) {
-        if (!options.message) throw new TypeError('Missing argument: message')
-        this.message = options.message
+        if(options.slash) {
+            if(!options.interaction) throw new TypeError("[djs-games] Interaction is not defined.")
+
+            this.message = options.interaction
+        } else {
+            if(!options.message) throw new TypeError("[djs-games] Message is not defined.")
+
+            this.message = options.message
+        }
+        this.slash = options.slash ? options.slash : false
         this.wrongGuess = options.wrongGuess
         this.correctGuess = options.correctGuess
-
-
     }
 
     start() {
+        let player;
+      
+        if(this.slash) {
+          player = this.message.user
+        } else {
+          player = this.message.author
+        }
 
         var options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
         var result = options[Math.floor(Math.random() * options.length)];
 
-        this.message.channel.send(`**Guess a number from 1 to 10!**`).then(async msg => {
-            const filter = m => m.author.id == this.message.author.id
+        this.message.reply(`**Guess a number from 1 to 10!**`).then(async msg => {
+            const filter = m => m.author.id == player.id
             this.message.channel.awaitMessages({ filter, max: 1 })
 
                 .then(collected => {
                     var guess = collected.first().content
                     if (isNaN(guess)) {
 
-                        this.message.channel.send(`**🤦‍♂️ You dimmadumbass, I need a number. Not that alphabet shit!** *Use the command again to play again*`)
+                        this.message.channel.send(`**🤦‍♂️ You dimmadumbass, I need a number. Not that alphabet sh#t!** *Use the command again to play again*`)
 
                     } else if (guess != result) {
 
@@ -32,7 +45,7 @@ class GuessTheNumber {
                             .then(collected => {
                                 var guess = collected.first().content
                                 if (isNaN(guess)) {
-                                    this.message.channel.send(`**🤦‍♂️ You You dimmadumbass, I need a number. Not that alphabet shit!** *Use the command again to play again*`)
+                                    this.message.channel.send(`**🤦‍♂️ You You dimmadumbass, I need a number. Not that alphabet sh#t!** *Use the command again to play again*`)
 
                                 } else if (guess === result) {
 
@@ -48,7 +61,7 @@ class GuessTheNumber {
                                             var guess = collected.first().content
                                             if (isNaN(guess)) {
 
-                                                this.message.channel.send(`**🤦‍♂️ You You dimmadumbass, I need a number. Not that alphabet shit!** *Use the command again to play again*`)
+                                                this.message.channel.send(`**🤦‍♂️ You You dimmadumbass, I need a number. Not that alphabet sh#t!** *Use the command again to play again*`)
                                             } else if (guess != result) {
 
                                                 this.message.channel.send(this.wrongGuess || `**Nope, that isn't the number. You only have 0 chances left.** *Number was ${result}*`)
